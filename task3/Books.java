4ab5fed02bb5f185a77495b76d080e9fdbbf3d7e
RomanZhukov-b <zukovroman232@gmail.com>
2023-06-14 18:04:26 +0300

    HW_3seminar

---
+++ task3/Books.java
@@ -0,0 +1,16 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Books {
+
+    private String name;
+    private String author;
+    private Double price;
+    private Integer year;
+    private Integer pgNum;
+
+
+
+}

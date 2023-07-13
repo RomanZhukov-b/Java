826335752065e45a26bda4caacce2d34c8a1ac69
RomanZhukov-b <zukovroman232@gmail.com>
2023-07-13 19:30:12 +0300

    calkulator

---
+++ HW_OOP/HW_5/calkulator/Present.java
@@ -0,0 +1,9 @@
+import org.Java.HW_OOP.HW_5.calkulator.view.View;
+
+public class Present {
+    public static void main(String[] args) {
+        View view = new View();
+        view.start();
+
+    }
+}
